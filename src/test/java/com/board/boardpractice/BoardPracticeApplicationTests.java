58171ac322abed8cc0e46d61d48b96229e46f718
PARK <seokscribe1970@gmail.com>
2022-11-20 22:01:26 +0900

    #7 - 스프링부트 프로젝트 시작
    스프링부트 초기화작업

---
+++ src/test/java/com/board/boardpractice/BoardPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.board.boardpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
