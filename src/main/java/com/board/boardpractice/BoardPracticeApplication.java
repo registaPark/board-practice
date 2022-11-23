e039764ec67457e41ba894ba9ba4b8d7dc4d51c5
PARK <seokscribe1970@gmail.com>
2022-11-23 23:40:20 +0900

    #16 - thymeleaf decoupled logic 설정
    yaml 프로퍼티 파일 + application에 어노테이션 추가

--- src/main/java/com/board/boardpractice/BoardPracticeApplication.java
+++ src/main/java/com/board/boardpractice/BoardPracticeApplication.java
@@ -2,7 +2,10 @@ package com.board.boardpractice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class BoardPracticeApplication {
 
